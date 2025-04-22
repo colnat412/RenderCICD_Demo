e699f39ac5252bad502a8db150138c5a0170a156
NguyenTanLoc <nguyentanloc1108@gmail.com>
2025-04-22 19:35:27 +0700

    Test CICD 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from GitHub Actions & Render Test!');
+  res.send('Hello from GitHub Actions & Render Test CICD!');
 });
 
 app.listen(port, () => {
